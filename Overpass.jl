82b6669640645ff7079a40ce009554ccf842a10a
Simon Simonsson <simonsson.simon@gmail.com>
2020-10-01 22:51:33 +0200

    made the arguments better in accordance with the nominatim api

--- Overpass.jl
+++ Overpass.jl
@@ -10,7 +10,7 @@ const END_MAP = "/map"
 
 Download OSM data for the given bounding box.
 """
-function map(min_lon, min_lat, max_lon, max_lat)
+function map(min_lat, max_lat, min_lon, max_lon)
 	HTTP.request(
 		"GET",
 		APIHOST * END_MAP,
