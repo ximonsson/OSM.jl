4a25226c54bad854b90d1bfece3845abec970790
Simon Simonsson <simonsson.simon@gmail.com>
2020-10-05 22:31:46 +0200

    querydata and get back through stream

--- Overpass.jl
+++ Overpass.jl
@@ -5,6 +5,15 @@ using HTTP
 const APIHOST = "http://overpass-api.de/api"
 const END_MAP = "/map"
 
+query(fn::Function, endpoint::AbstractString, q::AbstractString) =
+	HTTP.open(
+		"GET",
+		APIHOST * endpoint,
+		body = q,
+	) do io
+		fn(io)
+	end
+
 """
 	map(min_lon, min_lat, max_lon, max_lat)
 
@@ -18,7 +27,7 @@ function map(min_lat, max_lat, min_lon, max_lon)
 			:bbox => "$min_lon,$min_lat,$max_lon,$max_lat",
 		),
 	) |>
-	r -> r.body |> String
+	r -> r.body
 end
 
 end
