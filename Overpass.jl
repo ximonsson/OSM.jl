1880df4d73ac90c309a7e3b4840c217c3fd9cd46
Simon Simonsson <simonsson.simon@gmail.com>
2020-10-06 22:53:49 +0200

    fixing streaming of overpass response to more easily work with large response data

--- Overpass.jl
+++ Overpass.jl
@@ -2,17 +2,34 @@ module Overpass
 
 using HTTP
 
-const APIHOST = "http://overpass-api.de/api"
-const END_MAP = "/map"
+const APIHOST = "https://overpass-api.de/api"
+const ENDPOINT_MAP = "/map"
+const ENDPOINT_INTERPRETER = "/interpreter"
 
-query(fn::Function, endpoint::AbstractString, q::AbstractString) =
-	HTTP.open(
-		"GET",
-		APIHOST * endpoint,
-		body = q,
-	) do io
+function query(fn::Function, q::AbstractString)
+	@debug "querying" q
+	io = Base.BufferStream()
+	HTTP.request("POST", APIHOST * ENDPOINT_INTERPRETER, [], q, response_stream = io)
+	try
 		fn(io)
+	finally
+		close(io)
 	end
+end
+
+function bbox(fn::Function, min_lat, max_lat, min_lon, max_lon)
+	q = """
+	nwr($min_lat,$min_lon,$max_lat,$max_lon);
+	out;
+	"""
+	query(fn, q)
+end
+
+function bbox(min_lat, max_lat, min_lon, max_lon)
+	bbox(min_lat, max_lat, min_lon, max_lon) do io
+		read(io, String)
+	end
+end
 
 """
 	map(min_lon, min_lat, max_lon, max_lat)
@@ -22,7 +39,7 @@ Download OSM data for the given bounding box.
 function map(min_lat, max_lat, min_lon, max_lon)
 	HTTP.request(
 		"GET",
-		APIHOST * END_MAP,
+		APIHOST * ENDPOINT_MAP,
 		query = Dict(
 			:bbox => "$min_lon,$min_lat,$max_lon,$max_lat",
 		),
