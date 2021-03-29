6510b06131eba79c8515534ff8a930f1c184d7f2
Simon Simonsson <simonsson.simon@gmail.com>
2021-03-29 13:05:09 +0200

    moving coordinates translation to coords.jl

--- OSM.jl
+++ OSM.jl
@@ -4,7 +4,6 @@ using LibExpat, EzXML
 
 const Tag = Pair{Symbol,String}
 const Tags = Dict{Symbol,String}
-#const Tags = Vector{Tag}
 
 function Tag(el::EzXML.Node)
 	Symbol(replace(el["k"], ":" => "_")) => el["v"]
@@ -298,53 +297,7 @@ function parsefile(fp::AbstractString)
 	Data(nodes, ways)
 end
 
-
-"""
-	ENU(X, Y, Z, φ, λ)
-
-Convert between ECEF coordinate system and ENU using original geodetic coordinates as
-reference.
-
-TODO not sure what the X Y Z coordinates should be in this case.
-"""
-ENU(X, Y, Z, φ, λ) =
-	[
-		-sin(λ) cos(λ) 0;
-		-sin(φ)*cos(λ) -sin(φ)*cos(λ) cos(φ);
-		cos(φ)*cos(λ) cos(φ)*sin(λ) sin(φ);
-	] *
-	[X; Y; Z]
-
-"""
-	ENU(φ, λ, h = 0)
-
-Convert between geodetic coordinates and ENU. This is done by first converting to ECEF and
-then from ECEF to ENU, using the original geodetic as reference.
-"""
-ENU(φ, λ, h = 0) = ECEF(φ, λ, h) |> (x, y, z) -> ENU(x, y, z, φ, λ)
-
-""" Equatorial radius in meters. """
-const Re = 6378137.0 # equatorial radius
-
-""" Polar radius in meters. """
-const Rp = 6356752.3 # polar radius
-
-""" Constant used for converting to ECEF. """
-const e = 1 - Rp^2 / Re^2
-
-"""
-	ECEF(φ, λ, h = 0)
-
-Convert from geodetic coordinates to ECEF.
-"""
-function ECEF(φ, λ, h = 0)
-	N = Re / √(1 - e^2 * sin(φ)^2)
-
-	(N + h) * cos(φ) * cos(λ),
-	(N + h) * cos(φ) * sin(λ),
-	((1 - e^2) * N + h) * sin(φ)
-end
-
+include("coords.jl")
 include("Nominatim.jl")
 include("Overpass.jl")
 
